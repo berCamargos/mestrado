ea7a9b2343ce9bf45e4bac519a122eff73b70eec
Bernardo Camargos <ber.camargos@gmail.com>
2016-08-05 14:17:01 -0300

    Fixed the Code

--- testes/mpuTeste/mpuTeste.ino
+++ testes/mpuTeste/mpuTeste.ino
@@ -1,30 +1,32 @@
 #include <Wire.h>
 
-#define MPU_ADDR        0x68
-#define FIFO_READ_SIZE  6
-#define FIFO_READ_NUM   5
+/*FIFO_READ_SIZE should be a even divisor of 512*/
+#define FIFO_READ_SIZE  32    
 #define DEBUG           0
+#define FIFO_SENSORS    8+16+32+64
 
-void configAcc()
+uint8_t mpuAddr = 0;
+
+void configSensors(uint8_t addr)
 {
-  Wire.beginTransmission(MPU_ADDR);
-  Wire.write(28); /*Acc config*/
-  Wire.write(0);  /*scale +- 2g*/
+  Wire.beginTransmission(addr);
+  Wire.write(28);                   /*Acc config*/
+  Wire.write(0);                    /*scale +- 2g*/
   Wire.endTransmission(true);
 
-  Wire.beginTransmission(MPU_ADDR);
-  Wire.write(29); /*Acc config 2*/
-  Wire.write(4);  /*Freq 1kHz*/
+  Wire.beginTransmission(addr);
+  Wire.write(29);                   /*Acc config 2*/
+  Wire.write(4);                    /*Freq 1kHz*/
   Wire.endTransmission(true);
 
-  Wire.beginTransmission(MPU_ADDR);
-  Wire.write(25); /*Sample Rate*/
-  Wire.write(0);  /*1/4*/
+  Wire.beginTransmission(addr);
+  Wire.write(25);                   /*Sample Rate*/
+  Wire.write(0);                    /*max*/
   Wire.endTransmission(true);
 
-  Wire.beginTransmission(MPU_ADDR);
-  Wire.write(27); /*Sample Rate*/
-  Wire.write(0);  /*1/4*/
+  Wire.beginTransmission(addr);
+  Wire.write(27);                   /*Gy Config*/
+  Wire.write(0);                    /*scale +250dps*/
   Wire.endTransmission(true);
   
 
@@ -34,24 +36,27 @@ int8_t initMPU(uint8_t addr)
 {
   
   Wire.beginTransmission(addr);
-  Wire.write(0x1A); /*Set FIFO_MODE*/
-  Wire.write(64+1);    /*FIFO_MODE = 1 -> no overflow*/
+  Wire.write(26);               /*Set FIFO_MODE*/
+  Wire.write(64+1);             /*FIFO_MODE = 1 -> no overflow*/
   Wire.endTransmission(true);
   Wire.beginTransmission(addr);
-  Wire.write(0x23); /*Set FIFO_EN*/
-  Wire.write(8);    /*Enable only ACC*/
+  Wire.write(35);               /*Set FIFO_EN*/
+  Wire.write(FIFO_SENSORS);       /*Enable ACC, GyX, GyY, GyZ*/
   Wire.endTransmission(true);
   Wire.beginTransmission(addr);
-  Wire.write(0x6A);    /*USER_CTRL*/
-  Wire.write(64+4+1);  /*Enable FIFO, Reset Signals, reset Counter*/
+  Wire.write(106);              /*USER_CTRL*/
+  Wire.write(64+4+1);           /*Enable FIFO, Reset Signals, reset Counter*/
   Wire.endTransmission(true);
   Wire.beginTransmission(addr);
-  Wire.write(0x6B);
-  Wire.write(0);
+  Wire.write(107);              /*Power Management 1*/
+  Wire.write(0);                /*Internal 20MHz oscillator, no low power*/
   Wire.endTransmission(true);
-  
+}
+
+int8_t testMPUAddr(uint8_t addr)
+{
   Wire.beginTransmission(addr);
-  Wire.write(0x75); /*Counter register*/
+  Wire.write(0x75);             /*"Who am I" register*/
   Wire.endTransmission(false);
   Wire.requestFrom(addr,1,true);
   return Wire.read();
@@ -66,7 +71,7 @@ void setup() {
   uint8_t addr;
   for(addr = 10; addr < 0xFF; addr++)
   {
-    if(initMPU(addr) != -1)
+    if(testMPUAddr(addr) != -1)
     {
       break;
     }
@@ -75,42 +80,37 @@ void setup() {
   Serial.print("Found MPU on addr: ");
   Serial.println(addr);
 #endif
-  configAcc();
+  initMPU(addr);
+  configSensors(addr);
+  mpuAddr = addr;
 }
 
 void getData(uint16_t size)
 {
   int16_t data;
-  Wire.beginTransmission(MPU_ADDR);
-  Wire.write(0x74);
-  Wire.endTransmission(false);
   
   int16_t i,j,k;
 #if DEBUG
   unsigned long time = millis();
 #endif
-  Serial.println("-100000");
-  for(i = FIFO_READ_SIZE*FIFO_READ_NUM; i < 512; i+=FIFO_READ_SIZE*FIFO_READ_NUM)
+  for(i = 0; i < 512; i+=FIFO_READ_SIZE)
   {
-    Wire.beginTransmission(MPU_ADDR);
+    Wire.beginTransmission(mpuAddr);
     Wire.write(0x74);
     Wire.endTransmission(false);       
-    Wire.requestFrom(MPU_ADDR,FIFO_READ_SIZE*FIFO_READ_NUM,true);
-    for(j = 0; j < FIFO_READ_NUM; j++)
+    Wire.requestFrom(mpuAddr,FIFO_READ_SIZE,true);
+    for(j = 0; j < FIFO_READ_SIZE/2; j++)
     {
-      for(k = 0; k < FIFO_READ_SIZE/2; k++)
-      {
         data = Wire.read()<<8;
         data = data|Wire.read();  
         Serial.println(data);
-      }
     }
 #if DEBUG
     Serial.println("--");
 #endif
     Wire.endTransmission(true);    
   }
-  
+  Serial.println("-100000");
 #if DEBUG
   Serial.println(millis()-time);
 #endif
@@ -119,10 +119,10 @@ void getData(uint16_t size)
 void loop() {
   // put your main code here, to run repeatedly:
   uint16_t cntr;
-  Wire.beginTransmission(MPU_ADDR);
+  Wire.beginTransmission(mpuAddr);
   Wire.write(0x72); /*Counter register*/
   Wire.endTransmission(false);
-  Wire.requestFrom(MPU_ADDR,2,true);
+  Wire.requestFrom(mpuAddr,2,true);
   cntr = Wire.read()<<8|Wire.read();
   Wire.endTransmission(true);    
 
@@ -132,19 +132,18 @@ void loop() {
 #endif
   if(cntr >= 512)
   {
-
-    Wire.beginTransmission(MPU_ADDR);
+    Wire.beginTransmission(mpuAddr);
     Wire.write(0x23);
     Wire.write(0); /*Disable FIFO recordings*/
     Wire.endTransmission(true);    
     getData(1024);
-    Wire.beginTransmission(MPU_ADDR);
+    Wire.beginTransmission(mpuAddr);
     Wire.write(0x6A);
     Wire.write(64+4); /*Reset FIFO*/
-    Wire.endTransmission(true);    
-    Wire.beginTransmission(MPU_ADDR);
+    Wire.endTransmission(true);
+    Wire.beginTransmission(mpuAddr);
     Wire.write(0x23); /*Enable FIFO recording of ACC*/
-    Wire.write(8);
+    Wire.write(FIFO_SENSORS);
     Wire.endTransmission(true);  
   }
   //delay(1000);
--- testes/plotDataArduino.m
+++ testes/plotDataArduino.m
@@ -1,29 +1,73 @@
 delete(instrfindall)
 clear all
 close all
-s = serial('/dev/ttyUSB0','BaudRate',115200);
+s = serial('/dev/ttyUSB1','BaudRate',115200);
 fopen(s)
-maxsize = 1000
-a = zeros(maxsize,3)';
+NUM_SENSORS = 6;
+maxsizePlot = 10^4
+maxsizeHist = 10^6
 cnt = 0;
 b = [];
-real = [];
+tempAccX = [];
+tempAccY = [];
+tempAccZ = [];
+tempGyX = [];
+tempGyY = [];
+tempGyZ = [];
+tempEuler = [];
+X = [1;0;0;0];
+P = eye(4);
+acc = zeros(maxsizePlot,3);
+gy = zeros(maxsizePlot,3);
+euler = zeros(maxsizePlot,3);
 counter = 0;
 while 1==1
     b = [b;fscanf(s,'%d')];
     counter = counter + 1;
     if b(end) == -100000
-        real = [real;b(1:end-1)];
-        if size(real,1)>3*1000
-            real = real(end-3*1000:end);
+        if size(b,1) == 257
+            tempAccX = b(1:NUM_SENSORS:end);
+            tempAccY = b(2:NUM_SENSORS:end);
+            tempAccZ = b(3:NUM_SENSORS:end);
+
+            tempGyX  = b(4:NUM_SENSORS:end);
+            tempGyY  = b(5:NUM_SENSORS:end);
+            tempGyZ  = b(6:NUM_SENSORS:end);
+
+            minLen = min([size(tempAccX,1),size(tempAccY,1),size(tempAccZ,1),size(tempGyX,1),size(tempGyY,1),size(tempGyZ,1)]);
+
+            tempGyX = tempGyX(1:minLen);
+            tempGyY = tempGyY(1:minLen);
+            tempGyZ = tempGyZ(1:minLen);
+            tempAccX = tempAccX(1:minLen);
+            tempAccY = tempAccY(1:minLen);
+            tempAccZ = tempAccZ(1:minLen);
+            euler = [tempEuler(end:-1:1,:); euler(1:end-minLen,:)];
+
+            acc(:,1) = [tempAccX(end:-1:1); acc(1:end-(size(tempAccX,1)),1)];  
+            acc(:,2) = [tempAccY(end:-1:1); acc(1:end-(size(tempAccY,1)),2)];  
+            acc(:,3) = [tempAccZ(end:-1:1); acc(1:end-(size(tempAccZ,1)),3)];  
+
+            gy(:,1) = [tempGyX(end:-1:1); gy(1:end-(size(tempGyX,1)),1)];  
+            gy(:,2) = [tempGyY(end:-1:1); gy(1:end-(size(tempGyY,1)),2)];  
+            gy(:,3) = [tempGyZ(end:-1:1); gy(1:end-(size(tempGyZ,1)),3)];  
         end
         b = [];
     end
-    if counter > 100
-        if(size(real,1) > 100)
-            a = [real(1:3:end) real(2:3:end) real(3:3:end)];
-            plot(a)
-            axis([0 floor(size(real,1)/3) -2^15 2^15])
+    if counter > 2000
+        if(size(acc,1) > 100)
+
+            [tempEuler,X,P] = runKalmanOnSet(acc(),[tempGyX tempGyY tempGyZ],1000, X(:,end),P);
+            minLen = min([size(acc,1),size(gy,1)]);
+            subplot(311)
+            plot(acc)
+            axis([0 minLen -2^15 2^15])
+            subplot(312)
+            plot(gy)
+            axis([0 minLen -2^15 2^15])
+            subplot(313)
+            plot(euler)
+            axis([0 minLen -pi pi])
             drawnow();
         end
         counter  = 0;
